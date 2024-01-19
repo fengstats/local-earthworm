4d90bd3ef032dfae844595749ede0cb2931607b5
fengstats <feng2860984180@163.com>
2024-01-19 20:37:28 +0800

    fix: 完善磨砂背景问题

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,6 +7,8 @@ export default function MainLayout({
 }) {
   return (
     <div className="flex flex-col h-full">
+      {/* TODO: 使用伪元素 before 会在打卡分享图片时丢失，所以用一个实际 DOM 元素来做这件事 */}
+      <div className="noise" />
       <Header></Header>
       <main className="flex-grow">{children}</main>
       <Footer></Footer>
